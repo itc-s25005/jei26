5ccb3347678d5be571b8b9e18e3e005aa2cc5961
itc-s25005 <s25005@std.it-college.ac.jp>
2026-05-15 11:25:08 +0900

    課題

---
+++ src/d48/Q18_19.java
@@ -0,0 +1,16 @@
+package src.d48;
+
+public class Q18_19 {
+    public static void main(String[] args) {
+        double baseValue = 3.5;
+        int units = 3;
+
+        double width = baseValue * units;
+        System.out.println(width);
+    }
+
+    //int array1 = new int[5];
+    //int[] array2 = int[5];
+    int[] array3 = new int[5];
+    //int [][] array4 = new int[][5];
+}
---
+++ src/d48/Q20kadai.java
@@ -0,0 +1,9 @@
+package src.d48;
+
+public class Q20kadai {
+    public static void main(String[] args) {
+        char c = 0x30;
+        c += 18;
+        System.out.println(c);
+    }
+}
