71da3d441010ece3e2a994855c6860fb99cb6236
itc-s25005 <s25005@std.it-college.ac.jp>
2026-05-22 12:42:52 +0900

    課題

---
+++ src/d49/Q17.java
@@ -0,0 +1,10 @@
+package d49;
+
+public class Q17 {
+    public static void main(String[] args) {
+        int n = 1;
+
+        System.out.println(++n + " " + n--);
+        System.out.println(n);
+    }
+}
