c2abcbb2f502678a5b8c6cfd5912e9fe5886449b
itc-s25005 <s25005@std.it-college.ac.jp>
2026-05-29 10:10:42 +0900

    課題

--- src/d49/Q18Kadai.java
+++ src/d49/Q18Kadai.java
@@ -1,8 +1,13 @@
 package d49;
+/*
+Q18で与えられた変数を使い、実行結果が「4.0」となるようにしてください
+またなぜそうなるかも考えて、コード内コメントに記入してください。
+ */
+
 
 public class Q18Kadai {
     public static void main(String[] args) {
-        int total = 75;
+        int total = 60;
         int capacity = 30;
         int cargo = 2;
 
--- src/d49/Q20Kadai.java
+++ src/d49/Q20Kadai.java
@@ -1,4 +1,8 @@
 package d49;
+/*
+double型の２次元配列arrを5行10列で作成してください。
+また、配列のlengthを使いarrの行数を出力してください
+ */
 
 public class Q20Kadai {
     static void main(String[] args) {
