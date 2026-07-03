c7a14bccd5daea87c1af268b7533e9fdf9141a90
itc-s25005 <s25005@std.it-college.ac.jp>
2026-07-03 10:09:09 +0900

    課題

---
+++ src/d51/Q22.java
@@ -0,0 +1,12 @@
+package d51;
+
+public class Q22 {
+    public static void main(String[] args) {
+        view(false, "true");
+    }
+
+    public static void view(boolean b, String s) {
+        System.out.println(" " + b + s);
+    }
+
+}
