e486cfda6e9b3ade5b33d2a39d583e8835f28419
itc-s25005 <s25005@std.it-college.ac.jp>
2026-07-10 12:41:54 +0900

    課題

---
+++ src/d52/Q19.java
@@ -0,0 +1,10 @@
+package d52;
+
+public class Q19 {
+    static void main(String[] args) {
+        float f = 9.8f;
+        double d = 9.8;
+        System.out.println(f > d);
+        System.out.println(f + d);
+    }
+}
